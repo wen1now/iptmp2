790b656585248e9c1b88b7f69fe3c5e84b8c1b75
Max Tracey <maxtracey01@gmail.com>
2018-08-24 08:17:26 +1000

    Started coding index but realised I don't remember how to

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Log in</title>
+</head>
+<body>
+<h2>Critical Miss</h2>
+<form name="letmein" method="POST" action="index.php">
+	<input type="submit" value="Log In">
+</form>
+</body>
+</html>
