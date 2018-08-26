0068c91c1b6840480ffbb06cef734bfe11228c67
Max Tracey <maxtracey01@gmail.com>
2018-08-27 08:14:23 +1000

    added wen's changes to index

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start()
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,10 +8,15 @@
 </head>
 <body>
 <h2>Critical Miss</h2>
-<form name="letmein" method="POST" action="index.php">
-	Username: <input type="text">
-	Password: <input type="text">
+<form name="letmein" method="post" action="index.php">
+	Username: <input type="text" name="username"><br>
+	Password: <input type="password" name="password"><br>
 	<input type="submit" value="Log In">
 </form>
+
+<?php
+$_SESSION["username"] = $_POST["username"];
+$_SESSION["password"] = $_POST["password"]
+?>
 </body>
 </html>
