b1d72d3cf4c048b91c3cebd707dddc66d88a0691
Max Tracey <maxtracey01@gmail.com>
2018-08-27 13:50:31 +1000

    changed datatype of booleans to bit

--- data.sql
+++ data.sql
@@ -3,3 +3,4 @@ values ('password', 'BillyBob', '0000ff', 'default.jpg' ),
 ('letmein', 'Jimbro', 'ff0000', 'default.jpg'),
 ('1234', 'BarryBBenson', '00ff00', 'default.jpg'),
 ('hello', 'PineappleOnPizza', 'ffffff', 'default.jpg');
+
