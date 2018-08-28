6f2b248d38f6aad4f3621338dda330ade8bad2eb
Max Tracey <maxtracey01@gmail.com>
2018-08-29 08:55:27 +1000

    working on menu

--- menu.php
+++ menu.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if (!isset($_session['username'])) {
+ header("LOCATION: index.php");
+ }
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,8 +13,14 @@ body {
 }
 </style>
 </head>
-<body style="background-image:url(images/bgroundtiles.jpg)">
-	<h2>Critical Miss: Menu</h2>
+<body>
+	<?php
+	$username = $_SESSION['username'];
+	echo '<h3><font color="ffffff">Welcome '.$username.'</font></h3>';
+	 
+	?>
+	<h2><font color ='ffffff'>Critical Miss: Menu</font></h2>
+	<h3><font color = 'ffffff'></font></h3>
 	<form name="gomakeroom" method="post" action="">
 		<input type="submit" name="Log In" value="Log In">
 	</form>
@@ -19,6 +31,9 @@ body {
 
 	<form name="gocollection" method="post" action="">
 		<input type="submit" name="Create Account" value="Create Room">
-	</form
+	</form>
+	<form name="gologout" method="post" action="index.php">
+		<input type="submit" name="Sign Out" value="Log Out">
+	</form>
 </body>
 </html>
