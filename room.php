c870ac5c35c66d4a95e8a4ce79f3bfd115db42d2
wen1now <wen00@live.com.au>
2018-09-02 16:19:45 +1000

    styled up room a bit

--- room.php
+++ room.php
@@ -7,8 +7,11 @@ session_start();
 <html>
 <head>
 	<title></title>
+<link rel="stylesheet" href="window_thing/main.css">
+<link rel="stylesheet" href="roomstyle.css">
 </head>
 <body>
+	Protip: click and drag the top of the windows to reposition them
 <?php
 	/*
 	if (isset($_POST['submitcreate'])){
@@ -43,7 +46,7 @@ session_start();
 
 		//header('location: find.php');
 	} 
-	echo 'Room id: '.$_GET['roomid'].'<br>';
+	echo '<br><div>Room id: '.$_GET['roomid'].'</div>';
 	$user = $_SESSION['userid'];
 	$room = $_GET['roomid'];
 	require('connect.php');
@@ -51,17 +54,18 @@ session_start();
 	$qry = 'select pointer, username from users where userid in (select userid from characters where roomid = '
 		.$room.')';
 
-		echo('Users:<br>');
+		echo('<div class="drag users" onmousedown="totop(this)"><div class="topbar"><b>Users:</b></div>');
 	//this displays all users within the room in the colour they have chosen as their pointer
 	$userslist = mysqli_query($connection,$qry);
 	while (list($col1,$col2) = mysqli_fetch_row($userslist))
 		{	
 			echo '<font color ="'.$col1.'">'.$col2.'</font>, ' ;
 		}
+	echo '</div>';
 
 	$qry = 'select displayname,textcolour,send_date,blurb,username from messages,aliases,users where Messages.roomid = '.$room.' and messages.alias = aliases.aliasid and messages.userid = users.userid order by send_date';
 
-	echo('<br>Messages<br>');
+	echo('<div class="drag messages" onmousedown="totop(this)" style="left:50px;top:40px"><div class="topbar"><b>Messages:</b></div>');
 	$messagelist = mysqli_query($connection,$qry);
 	while (list($col1,$col2,$col3,$col4,$col5) = mysqli_fetch_row($messagelist))
 		{
@@ -78,10 +82,12 @@ session_start();
 		}
  ?>
  	<form name="sendmessage" method="post" action ="room.php">
- 		Send<input type="text" name="message">as:<input type="text" name="alias">
+ 		Send<input type="text" name="message"><br>as:<input type="text" name="alias">
  		<input type="submit" name="submitmessage" value="send">
  	</form>
 
 
+<script type="text/javascript" src="window_thing/interact.js"></script>
+<script type="text/javascript" src="window_thing/main.js"></script>
 </body>
 </html>
