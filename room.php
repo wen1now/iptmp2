4dc6ad74c806232d1e165b7693963fe6199a44eb
Max Tracey <maxtracey01@gmail.com>
2018-09-02 15:18:15 +1000

    started work on the room displaying the aliases

--- room.php
+++ room.php
@@ -51,7 +51,7 @@ session_start();
 	$qry = 'select pointer, username from users where userid in (select userid from characters where roomid = '
 		.$room.')';
 
-
+		echo('Users:<br>');
 	//this displays all users within the room in the colour they have chosen as their pointer
 	$userslist = mysqli_query($connection,$qry);
 	while (list($col1,$col2) = mysqli_fetch_row($userslist))
@@ -59,14 +59,23 @@ session_start();
 			echo '<font color ="'.$col1.'">'.$col2.'</font>, ' ;
 		}
 
-	$qry = 'select displayname,textcolour,send_date,blurb from messages,aliases where Messages.roomid = '.$room.' and messages.alias = aliases.aliasid order by send_date';
+	$qry = 'select displayname,textcolour,send_date,blurb,username from messages,aliases,users where Messages.roomid = '.$room.' and messages.alias = aliases.aliasid and messages.userid = users.userid order by send_date';
 
+	echo('<br>Messages<br>');
 	$messagelist = mysqli_query($connection,$qry);
-	while (list($col1,$col2,$col3,$col4) = mysqli_fetch_row($messagelist))
+	while (list($col1,$col2,$col3,$col4,$col5) = mysqli_fetch_row($messagelist))
 		{
-			echo '<h4><font color ="'.$col3.'">'.$col1.' said '.$col4.' on '.$col3.'</font></h4><br>';
+			echo '<h4><font color ="'.$col2.'">'.$col5.' as '.$col1.' said '.$col4.' on '.$col3.'</font></h4>';
 		}
 
+	$qry = 'select displayname,textcolour,username from Aliases,Users where aliases.owner = Users.userid and roomid = '.$room;
+
+	echo('aliases:<br>');
+	$aliaslist = mysql_query($connection,$qry);
+	while (list($col1,$col2,$col3) = mysqli_fetch_row($aliaslist))
+		{
+			echo '<h4><font color ="'.$col2.'">'.$col3.' as '.$col1.'</font></h4>';
+		}
  ?>
  	<form name="sendmessage" method="post" action ="room.php">
  		Send<input type="text" name="message">as:<input type="text" name="alias">
