a589fb1ab265451c4728e8851f3c476608dfe97d
Max Tracey <maxtracey01@gmail.com>
2018-09-02 11:15:36 +1000

    edited room

--- room.php
+++ room.php
@@ -19,16 +19,15 @@ session_start();
 	$room = $_GET['roomid'];
 	require('connect.php');
 
-	$qry = 'select * from users where user in (select userid from characters where roomid = "'
+	$qry = 'select pointer, username from users where user in (select userid from characters where roomid = "'
 		.$room.'")';
 
 
 	//this displays all users within the room in the colour they have chosen as their pointer
 	$userslist = mysqli_query($connection,$qry);
-	echo ''.$userslist;
-	while (list($col1,$col2,$col3,$col4,$col5) = mysqli_fetch_row($userslist))
+	while (list($col1,$col2) = mysqli_fetch_row($userslist))
 		{	
-			echo '<h4><font color ="'.$col4.'">'.$col3.'</font></h4><br>' ;
+			echo '<h4><font color ="'.$col1.'">'.$col2.'</font></h4><br>' ;
 		}
 
 	$qry = 'select displayname,textcolour,send_date,blurb from messages,aliases where Messages.roomid = '.$room.' and messages.alias = aliases.aliasid order by send_date';
